7bf8112a8bb7b6448c39429bd96785fea226c95f
bengold1999 <bengold1999@gmail.com>
2024-05-13 17:33:40 +0300

    add style to page

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -1,7 +1,7 @@
 export function AppHeader(){
-    return <section>
-        <div>
-            <h4>Bg</h4>
+    return <section className="appHeader">
+        <div className="card_logo">
+            <h4>BG</h4>
         </div>
     </section>
 }
