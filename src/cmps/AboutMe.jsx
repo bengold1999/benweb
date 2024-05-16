cf48a5e3faff06c506c9358ecde9a6d753a12a96
Ben Goldberger <bengold1999@gmail.com>
2024-05-16 15:06:55 +0300

    Add about me style

--- src/cmps/AboutMe.jsx
+++ src/cmps/AboutMe.jsx
@@ -1,8 +1,9 @@
 export function AboutMe(){
     return <section className="about-me">
         <h3>About me</h3>
-        <section>
+        <section className="-contact">
         <p>Hi, I'm Ben. I am a full stack developer and a 3D generalist, skilled in frameworks like React, Angular, and Vue, and proficient in JavaScript, TypeScript, and CSS/SCSS. In the 3D realm, I work with tools like Blender, Substance Painter, CC4, and iClone8, and have experience with game engines like Unreal Engine and Unity. Passionate about web development and digital art, I strive to blend technical expertise with creative vision. I continuously seek opportunities to innovate and collaborate, bringing extraordinary projects to life in both digital and artistic spheres. Let's connect!</p>
+    <img src="https://res.cloudinary.com/dheh8zkmv/image/upload/v1715859261/WhatsApp_Image_2023-01-06_at_19.51.54_t38zxp.png" alt="" />
     </section>
     </section>
 }
