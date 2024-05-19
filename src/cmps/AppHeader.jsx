8a3064afd7f4536b61d8d04cd15f52e5930f6b34
Ben Goldberger <bengold1999@gmail.com>
2024-05-19 14:34:53 +0300

    Add active to app header

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -1,18 +1,39 @@
+import React, { useState } from 'react';
+
 export function AppHeader({ scrollToComponent }) {
+    const [activeSection, setActiveSection] = useState('');
+
+    const handleClick = (section) => {
+        setActiveSection(section);
+        scrollToComponent(section);
+    };
+
     return (
         <section className="appHeader">
-            <div className="card_logo" onClick={() => scrollToComponent('home')}>
+            <div 
+                className={`card_logo ${activeSection === 'home' ? 'active' : ''}`} 
+                onClick={() => handleClick('home')}
+            >
                 <h4 className="fa-solid fa-house"></h4>
             </div>
-            <div className="card_logo" onClick={() => scrollToComponent('about')}>
+            <div 
+                className={`card_logo ${activeSection === 'about' ? 'active' : ''}`} 
+                onClick={() => handleClick('about')}
+            >
                 <h4 className="fa-solid fa-address-card"></h4>
             </div>
-            <div className="card_logo" onClick={() => scrollToComponent('projects')}>
+            <div 
+                className={`card_logo ${activeSection === 'projects' ? 'active' : ''}`} 
+                onClick={() => handleClick('projects')}
+            >
                 <h4 className="fa-solid fa-file"></h4>
             </div>
-            <div className="card_logo" onClick={() => scrollToComponent('contact')}>
+            <div 
+                className={`card_logo ${activeSection === 'contact' ? 'active' : ''}`} 
+                onClick={() => handleClick('contact')}
+            >
                 <h4 className="fa-solid fa-message"></h4>
             </div>
         </section>
-    )
+    );
 }
