84bcf58625488823aee4431c8809eb23462f21f6
bengold1999 <bengold1999@gmail.com>
2024-05-22 15:47:48 +0300

    Add more style

--- src/cmps/AboutMe.jsx
+++ src/cmps/AboutMe.jsx
@@ -35,7 +35,8 @@ export function AboutMe() {
               <img className="Ben-img" src="https://res.cloudinary.com/dheh8zkmv/image/upload/v1715859261/WhatsApp_Image_2023-01-06_at_19.51.54_t38zxp.png" alt="Profile Image 626x626" />
             </div>
             <div className="right">
-              <div className="text">I'm Ben and I'm a <span className="typing-2"></span></div>
+              <div className="text">I'm Ben and I'm a </div>
+              <div className="text"><span className="typing-2"></span></div>
               <p>Hi, I'm Ben. I am a full stack developer and a 3D generalist, skilled in frameworks like React, Angular, and Vue, and proficient in JavaScript, TypeScript, and CSS/SCSS. In the 3D realm, I work with tools like Blender, Substance Painter, CC4, and iClone8, and have experience with game engines like Unreal Engine and Unity. Passionate about web development and digital art, I strive to blend technical expertise with creative vision. I continuously seek opportunities to innovate and collaborate, bringing extraordinary projects to life in both digital and artistic spheres. Let's connect!.</p>
 
               <div className="text">Why Work With Me</div>
