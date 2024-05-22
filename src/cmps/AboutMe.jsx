fa196dad37c971ea10215e5f298966c5d9d859b5
bengold1999 <bengold1999@gmail.com>
2024-05-22 21:41:22 +0300

    Add more style to about

--- src/cmps/AboutMe.jsx
+++ src/cmps/AboutMe.jsx
@@ -2,16 +2,94 @@
 
 import { useEffect } from "react";
 import Typed from 'typed.js';
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export function AboutMe() {
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2500,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />
+  };
+
+  // Custom arrow components
+  function SampleNextArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        className={className}
+        style={{ ...style, display: "block" }}
+        onClick={onClick}
+      />
+    );
+  }
+
+  function SamplePrevArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        className={className}
+        style={{ ...style, display: "block" }}
+        onClick={onClick}
+      />
+    );
+  }
+
+  const imgs_3d = [
+    {
+      img: "https://res.cloudinary.com/dheh8zkmv/image/upload/v1716396517/gujzrjkrvjdye3zryyyq.webp",
+      title: "blender",
+    },
+    {
+      img: "https://res.cloudinary.com/dheh8zkmv/image/upload/v1716396517/mh5uhsn05fgxtakiw3h3.webp",
+      title: "Substance painter",
+    },
+    {
+      img: "https://res.cloudinary.com/dheh8zkmv/image/upload/v1716396518/wzwql9l5rnhzcouixfdy.webp",
+      title: "Unity & Unreal",
+    },
+
+    // {
+    //   img: "https://res.cloudinary.com/dheh8zkmv/image/upload/v1716041395/hobuahl7aeczk1nssp4d.png",
+    //   title: "CC4 & Iclone8",
+
+    // },
+  ];
+  const imgs = [
+    {
+      img: "https://res.cloudinary.com/dheh8zkmv/image/upload/v1716396517/huvxhtamjrgixwhqblwh.webp",
+      title: "Angular & Vue & React",
+    },
+    {
+      img: "https://res.cloudinary.com/dheh8zkmv/image/upload/v1716396518/xninixvqkxurmdld1pwl.webp",
+      title: "Js & Ts",
+    },
+    {
+      img: "https://res.cloudinary.com/dheh8zkmv/image/upload/v1716396517/dyxnkqdphn8scylgzx4e.webp",
+      title: "MongoDB & express",
+    },
+
+    {
+      img: "https://res.cloudinary.com/dheh8zkmv/image/upload/v1716397906/tzfdxn2h93xq9ux2vbjv.webp",
+      title: "css & scss",
+
+    },
+  ];
 
   useEffect(() => {
     const typed = new Typed('.typing-2', {
       strings: [
         'Developer',
         '3D Artist',
-        'Passionate Coder',
-        'Problem Solver',
+        'Coder',
+        'Solver',
 
       ],
       typeSpeed: 150,
@@ -32,16 +110,37 @@ export function AboutMe() {
 
           <div className="about-content ">
             <div className="left ">
-              <img className="Ben-img" src="https://res.cloudinary.com/dheh8zkmv/image/upload/v1715859261/WhatsApp_Image_2023-01-06_at_19.51.54_t38zxp.png" alt="Profile Image 626x626" />
             </div>
             <div className="right">
+              <img className="Ben-img" src="https://res.cloudinary.com/dheh8zkmv/image/upload/v1715859261/WhatsApp_Image_2023-01-06_at_19.51.54_t38zxp.png" alt="Profile Image 626x626" />
               <div className="text">I'm Ben and I'm a </div>
               <div className="text"><span className="typing-2"></span></div>
-              <p>Hi, I'm Ben. I'm a full stack developer and 3D generalist, skilled in React, Angular, Vue, JavaScript, TypeScript, CSS/SCSS, Blender, and Unreal Engine. I blend technical expertise with creative vision in web development and digital art.</p>
-
+              <div className="cool-img">
+                <h3> FullStack Developer skills</h3>
+                <Slider {...settings}>
+                  {imgs.map((project, index) => (
+                    <div className="card" key={index}>
+                      <div className="box">
+                        <img src={project.img} alt={project.title} />
+                        <div className="text">{project.title}</div>
+                      </div>
+                    </div>
+                  ))}
+                </Slider>
+                <h3> 3D Generalist skills</h3>
+                <Slider {...settings}>
+                  {imgs_3d.map((project, index) => (
+                    <div className="card" key={index}>
+                      <div className="box">
+                        <img src={project.img} alt={project.title} />
+                        <div className="text">{project.title}</div>
+                      </div>
+                    </div>
+                  ))}
+                </Slider>
+              </div>
               <div className="text">Why Work With Me</div>
-              <p>I'm a great communicator who takes the time to fully understand and address customer problems. As a skilled and collaborative teammate, I bring both technical expertise and creative vision to every project.</p>
-
+              <p>I excel in communication, bringing both technical expertise and creative vision to every project as a skilled and collaborative teammate.</p>
               <a href="https://drive.google.com/file/d/1Q96uR4HSDWYDioYqlCFvcOUVCUU31Vnt/view?usp=sharing" target="blank">Download CV</a>
             </div>
           </div>
