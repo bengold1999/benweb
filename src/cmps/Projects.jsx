6892bba7a15978f1ba633365a174c09a3513dcec
bengold1999 <bengold1999@gmail.com>
2024-05-22 15:28:04 +0300

    Delete dots

--- src/cmps/Projects.jsx
+++ src/cmps/Projects.jsx
@@ -5,7 +5,7 @@ import "slick-carousel/slick/slick-theme.css";
 
 export function Projects() {
   const settings = {
-    dots: true,
+    dots: false,
     infinite: true,
     speed: 500,
     slidesToShow: 1,
